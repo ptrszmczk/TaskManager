183f9855b85b0a2dc8563763895d40502edcddf4
ptrszmczk <ptrszmczk@gmail.com>
2022-11-06 17:51:41 +0100

    Add project files.

---
+++ TaskManager/Controllers/TaskControler.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models;
+using TaskManager.Repositories;
+
+namespace TaskManager.Controllers
+{
+    public class TaskControler : Controller
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public TaskControler(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        // GET: TaskControler
+        public ActionResult Index()
+        {
+            return View(_taskRepository.GetAllActive());
+        }
+
+        // GET: TaskControler/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(_taskRepository.Get(id));
+        }
+
+        // GET: TaskControler/Create
+        public ActionResult Create()
+        {
+            return View(new TaskModel());
+        }
+
+        // POST: TaskControler/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TaskModel taskModel)
+        {
+            _taskRepository.Add(taskModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskControler/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(_taskRepository.Get(id));
+        }
+
+        // POST: TaskControler/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, TaskModel taskModel)
+        {
+            _taskRepository.Update(id, taskModel);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskControler/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(_taskRepository.Get(id));
+        }
+
+        // POST: TaskControler/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, TaskModel taskModel)
+        {
+            _taskRepository.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: TaskControler/Done
+        public ActionResult Done(int id)
+        {
+            TaskModel task = _taskRepository.Get(id);
+            task.Done = true;
+            _taskRepository.Update(id, task);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TaskManager/Models/TaskManagerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
